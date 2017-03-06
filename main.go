c997203978aad8b6492bba90900bfb3313386e52
Fabrice <fabrice.vaillant@centraliens.net>
2017-03-06 17:55:31 +0100

    Improves the random generator

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import "flag"
 
 var mapPath string
+var useRand bool
 
 func init() {
 	flag.StringVar(&mapPath, "map", "maps/testmap.xml", "path to the map to load")
+	flag.BoolVar(&useRand, "rand", false, "use a randomly generated map")
 }
 
 func main() {
 	flag.Parse()
 	var names [2]string
-	s := server{newMap(mapPath), names}
+	var m *Map
+	if !useRand {
+		m = newMap(mapPath)
+	} else {
+		m = generate(mapPath, 10, 10, 4, 6)
+	}
+	s := server{m, names}
 	go s.run()
 	startWebApp(s.Map)
 }
--- map_generator.go
+++ map_generator.go
@@ -4,12 +4,77 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"math/rand"
+	"sort"
+	"time"
 )
 
 // generate a random map of size Rows x Columns
-// with humans being the number of group of humans
+// with humans being half the number of group of humans (for symetrie)
 // and monsters being the number of monster
-func generate(filename string, Row, Columns, humans, monsters int) {
+func generate(filename string, Rows, Columns, humans, monster int) *Map {
+	rand.Seed(time.Now().UnixNano())
+	m := &Map{Rows: Rows, Columns: Columns}
+	m.cells = make([]cell, m.Columns*m.Rows)
+	var i int
+	for x := 0; x < m.Columns; x++ {
+		for y := 0; y < m.Rows; y++ {
+			m.cells[i].X = x
+			m.cells[i].Y = y
+			i++
+		}
+	}
+	// get a symetrie function
+	var f func(x, y, rows, columns int) (int, int)
+	switch rand.Intn(3) {
+	case 0:
+		f = axial
+	case 1:
+		f = center
+	case 2:
+		f = diagonal
+	}
+	// Returns a permutation of n integer
+	perm := rand.Perm(m.Columns * m.Rows)
+	// Will crash if human is bigger thant the size of the grid
+	for _, i := range perm[:humans] {
+		c := m.cells[i]
+		c.Count = 5 + rand.Intn(monster)
+		m.set(c)
+		m.humans = append(m.humans, i)
+		// there is a risk that the random sequence contains
+		// a cell and it's symetrical
+		// but whatever for now
+		// Also no axial symetrie
+		sym := c
+		sym.X, sym.Y = f(c.X, c.Y, m.Rows, m.Columns)
+		sym.Count = c.Count
+		j := m.set(sym)
+		m.humans = append(m.humans, j)
+	}
+	sort.Ints(m.humans)
+	// Random starter cell
+	i = rand.Intn(m.Columns * m.Rows)
+	c := m.cells[i]
+	// Make sure that the random cell we picked isn't it's own symetrique
+	// otherwise we won't get any ennemies
+	for {
+		symX, symY := f(c.X, c.Y, m.Rows, m.Columns)
+		if symX != c.X || symY != c.Y {
+			break
+		}
+	}
+	c.Count = monster
+	c.kind = wolf
+	m.set(c)
+	m.monster[0] = []int{i}
+	sym := c
+	sym.X, sym.Y = f(c.X, c.Y, m.Rows, m.Columns)
+	sym.Count = monster
+	sym.kind = vamp
+	j := m.set(sym)
+	m.monster[1] = []int{j}
+	return m
 }
 
 func (m *Map) toXML(w io.Writer) {
@@ -28,4 +93,17 @@ func (m *Map) toXML(w io.Writer) {
 		w.Write([]byte(fmt.Sprintf("\t<Vampires X=\"%d\" Y=\"%d\" Count=\"%d\"/>\n", c.X, c.Y, c.Count)))
 	}
 	w.Write([]byte("</Map>\n"))
+}
+
+// Those are symetri function they take X, and Y value of a cell, the rows and height dimension
+// and return the X and Y value of the symetrical cell
+
+func diagonal(X, Y int, rows, columns int) (int, int) {
+	return Y, X
+}
+func center(X, Y int, rows, columns int) (int, int) {
+	return rows - 1 - Y, columns - 1 - X
+}
+func axial(X, Y int, rows, columns int) (int, int) {
+	return X, rows - 1 - Y
 }
