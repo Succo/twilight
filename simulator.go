fdf77f7f43388390ecebd8bb6f4709ec886873cf
Fabrice <fabrice.vaillant@centraliens.net>
2017-03-06 22:55:53 +0100

    Adds the random function from the C# version
    It's the safest way to make sure the logic is the same

--- simulator.go
+++ simulator.go
@@ -3,23 +3,7 @@ package main
 import "math/rand"
 
 func simulateMonsterFight(E1, E2 int) (survivor int, victory bool) {
-	if float64(E1) > 1.5*float64(E2) {
-		// Instant Win
-		return E1, true
-	}
-	// FIGHT
-	var P float64
-	if E1 == E2 {
-		P = 0.5
-	} else if E1 < E2 {
-		P = float64(E1) / float64(2*E2)
-	} else {
-		P = float64(E1)/float64(E2) - 0.5
-	}
-	if P > 1.0 {
-		P = 1.0
-	}
-
+	P := getProba(E1, E2, false)
 	// FIGHT
 	if rand.Float64() < P {
 		// Victory
@@ -41,23 +25,7 @@ func simulateMonsterFight(E1, E2 int) (survivor int, victory bool) {
 }
 
 func simulateHumanFight(E1, E2 int) (survivor int, victory bool) {
-	if float64(E1) > float64(E2) {
-		// Instant Win
-		return E1 + E2, true
-	}
-	// FIGHT
-	var P float64
-	if E1 == E2 {
-		P = 0.5
-	} else if E1 < E2 {
-		P = float64(E1) / float64(2*E2)
-	} else {
-		P = float64(E1)/float64(E2) - 0.5
-	}
-	if P > 1.0 {
-		P = 1.0
-	}
-
+	P := getProba(E1, E2, true)
 	// FIGHT
 	if rand.Float64() < P {
 		// Victory
@@ -75,5 +43,38 @@ func simulateHumanFight(E1, E2 int) (survivor int, victory bool) {
 			}
 		}
 		return survivor, false
+	}
+}
+
+// getProba reimplements the getProba logic from Board.cs in the C# implementation
+func getProba(E1, E2 int, involveHumans bool) float64 {
+	if E1 == E2 {
+		return 0.5
+	}
+	var cste float64
+	if involveHumans {
+		cste = 1
+	} else {
+		cste = 1.5
+	}
+
+	// True by property
+	if float64(E1) >= cste*float64(E2) {
+		return 1
+	}
+
+	var x0, y0 float64
+	x1 := float64(E2)
+	y1 := 0.5
+	if E1 < E2 {
+		x0 = 0
+		y0 = 0
+		return (y0 - y1) / (x0 - x1) * float64(E1)
+	} else {
+		x0 = cste * float64(E2)
+		y0 = 1
+		m := (y0 - y1) / (x0 - x1)
+		c := 1 - m*x0
+		return m*float64(E2) + c
 	}
 }
