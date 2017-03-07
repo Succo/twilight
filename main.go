3f9fed08a21b1c7ee54b8275309a32e6997d1779
Fabrice <fabrice.vaillant@centraliens.net>
2017-03-07 12:12:34 +0100

    Improves rendering of the help message

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ var humans int
 var monster int
 
 func init() {
-	flag.StringVar(&mapPath, "map", "", "path to the map to load")
-	flag.BoolVar(&useRand, "rand", false, "use a randomly generated map (or save if randomly generating)")
+	flag.StringVar(&mapPath, "map", "", "path to the map to load (or save if randomly generating)")
+	flag.BoolVar(&useRand, "rand", false, "use a randomly generated map")
 	flag.IntVar(&rows, "rows", 10, "total number of rows")
 	flag.IntVar(&columns, "columns", 10, "total number of columns")
 	flag.IntVar(&humans, "humans", 16, "quantity of humans group")
