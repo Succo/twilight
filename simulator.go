6dbd429b8c5ad1018432121f0e769b46c94267e4
Lizzaroc <alexandre.prot@student.ecp.fr>
2018-03-06 12:29:52 +0100

    fixing equal number fight probability

--- simulator.go
+++ simulator.go
@@ -48,9 +48,6 @@ func simulateHumanFight(E1, E2 int) (survivor int, victory bool) {
 
 // getProba reimplements the getProba logic from Board.cs in the C# implementation
 func getProba(E1, E2 int, involveHumans bool) float64 {
-	if E1 == E2 {
-		return 0.5
-	}
 	var cste float64
 	if involveHumans {
 		cste = 1
